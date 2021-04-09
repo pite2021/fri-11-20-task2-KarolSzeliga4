73c2db0899053752dd55c2ee691747fd4c01679e
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-04-09 12:59:04 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,26 +1,39 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+
+class Transaction:
+  def __init__(self, client_give, client_take, money):
+    self.client_give = client_give
+    self.client_take = client_take
+    self.money = money
+
+class Client:
+  def __init__(self, name, bank_name, pin, start_money):
+    self.name = name
+    self.bank_name = bank_name
+    self.pin = pin
+    self.wallet = 0.0
+    self.tans_history = []
+
+class Bank:
+  def __init__(self, name):
+    self.name = name
+    self.client_list = []
+
+  def add_client(self,client):
+    self.client_list.push(client)
+
+if __name__ == "__main__":
+  print("Start by add first bank\n Bank name:")
+  bank_name = input()
+  Bank1 = Bank(bank_name)
+  print("Bank was created!")
+  print("add some clients")
+
+  print("first client name:")
+  cl_name = input()
+  print("client start money")
+  cl_money = input()
+  client = Client(cl_name,Bank1.name,1234,cl_money)
+  Bank.add_client(client)
+  print("Client added!")
+  print("cnd...")
